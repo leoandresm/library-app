10ea14a8975dc44fb8df02441565e605348a2a6f
Leo Andres Moreno Caicedo <leo.moreno@innovapps.co>
2019-05-04 08:26:12 -0500

    Se agrega configuracion electron

--- src/app/shared/services/book.service.ts
+++ src/app/shared/services/book.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private booksUrl = 'http://localhost:8100/api/books';
+  private booksUrl = `${environment.apiUrl}api/books`;
 
   constructor(private http: HttpClient) { }
 
